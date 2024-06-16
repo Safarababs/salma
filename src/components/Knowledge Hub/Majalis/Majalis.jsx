7055b14d8dacac76e881fcb44fbfdea6a426a124
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2024-06-16 05:47:36 +0300

    safar

--- src/components/Knowledge Hub/Majalis/Majalis.jsx
+++ src/components/Knowledge Hub/Majalis/Majalis.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const Majalis = () => {
   return (
     <section style={{ paddingTop: "5rem" }}>
-      <div class="container-fluid home py-5">
-        <div class="container py-5">
+      <div className="container-fluid home py-5">
+        <div className="container py-5">
           <div
-            class="text-center mx-auto mb-5 wow fadeIn"
+            className="text-center mx-auto mb-5 wow fadeIn"
             data-wow-delay="0.1s"
             style={{ maxWidth: "700px" }}
           >
-            <h1 class="display-3">Majalis will be avaiable soon</h1>
+            <h1 className="display-3">Majalis will be avaiable soon</h1>
           </div>
         </div>
       </div>
--- src/components/Knowledge Hub/Videos/Videos.jsx
+++ src/components/Knowledge Hub/Videos/Videos.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import videoFile from "../Videos/Salma Batool.mp4";
+import videoFile1 from "../Videos/salma1.mp4";
+import videoFile2 from "../Videos/salma2.mp4";
 
 const Videos = () => {
   return (
     <section style={{ paddingTop: "5rem" }}>
-      <div class="container-fluid home py-5">
-        <div class="container py-5">
+      <div className="container-fluid home py-5">
+        <div className="container py-5">
           <div
-            class="text-center mx-auto mb-5 wow fadeIn"
+            className="text-center mx-auto mb-5 wow fadeIn"
             data-wow-delay="0.1s"
             style={{ maxWidth: "700px" }}
           >
-            <h1 class="display-3">Videos Section</h1>
+            <h1 className="display-3">Videos Section</h1>
           </div>
-          <video width="640" height="360" controls>
-            <source src={videoFile} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          <div className="videos">
+            <div className="video-wrapper">
+              <video className="video-item" controls>
+                <source src={videoFile} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <div className="video-wrapper">
+              <video className="video-item" controls>
+                <source src={videoFile1} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <div className="video-wrapper">
+              <video className="video-item" controls>
+                <source src={videoFile2} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
         </div>
       </div>
     </section>
